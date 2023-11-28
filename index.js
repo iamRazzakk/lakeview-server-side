b3d216d6d2b97961ce31b8175ac0da8614cbfe1b
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-11-29 02:45:35 +0600

    show announcement for user and and post by admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
@@ -31,7 +31,14 @@ async function run() {
         const userCollection = client.db("lakeviewDB").collection("users");
         const agreementCollection = client.db("lakeviewDB").collection("agreements");
         const announcementsCollection = client.db("lakeviewDB").collection("announcements");
-
+        const couponCollection = client.db("lakeviewDB").collection("coupon");
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20h' })
+            res.send({ token })
+        })
+        // apartments related api
         app.get('/apartments', async (req, res) => {
             const result = await appertmentCollection.find().toArray();
             res.send(result)
@@ -47,6 +54,10 @@ async function run() {
             res.send(result)
         })
         // for user
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
@@ -57,15 +68,60 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        });
+        // 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
         // for announcement
         app.post('/announcements', async (req, res) => {
             const announcement = req.body;
             const result = await announcementsCollection.insertOne(announcement);
             res.send(result);
         })
+        app.get('/announcements', async (req, res) => {
+            const result = await announcementsCollection.find().toArray();
+            res.send(result)
+        })
+        // for coupon
+        app.post('/coupon', async (req, res) => {
+            const coupon = req.body;
+            const result = await couponCollection.insertOne(coupon);
+            res.send(result);
+        })
+        app.get('/coupon', async (req, res) => {
+            const result = await couponCollection.find().toArray();
+            res.send(result)
+        })
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+        // Send a ping to confirm a successful c onnection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
